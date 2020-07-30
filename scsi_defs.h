a514e31769764001961ce5b506009ac4399b77e5
Derell Licht <derelict@comcast.net>
2020-07-30 07:37:22 -0700

    code cleanup

--- scsi_defs.h
+++ scsi_defs.h
@@ -1,118 +1,303 @@
-/*++
+/*
+    WHaC - Windows Hardware Collector.
+    Hardware information gathers application.
+    Check the possibility to migrate this hardware to Linux or any
+     other free software operating system.
+    Copyright (C) 2009 Erez Geva
 
-Copyright (c) 1990-2000 Microsoft Corporation, All Rights Reserved
+    This file is part of WHaC.
 
-Module Name:
+    WHaC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    enumcd.h
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
 
-Abstract:
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __scsi_h
+#define __scsi_h
 
-    This file includes data declarations for the Enumcd
-
-Author:
-
-    Ramu Ramanathan     05/15/2000
-
-Notes:
-
-Revision History:
-
-
---*/
-
-#ifndef _ENUMCD_H_
-#define _ENUMCD_H_
-
+/****************************************************************************/
+// From inc/ddk/scsi.h
 
 //
 // Command Descriptor Block constants.
 //
 
-#define CDB6GENERIC_LENGTH         6
-#define CDB10GENERIC_LENGTH        10
+#define CDB6GENERIC_LENGTH                   6
+#define CDB10GENERIC_LENGTH                  10
+#define CDB12GENERIC_LENGTH                  12
 
+#define SETBITON                             1
+#define SETBITOFF                            0
+
+//
+// Mode Sense/Select page constants.
+//
+
+#define MODE_PAGE_VENDOR_SPECIFIC       0x00
+#define MODE_PAGE_ERROR_RECOVERY        0x01
+#define MODE_PAGE_DISCONNECT            0x02
+#define MODE_PAGE_FORMAT_DEVICE         0x03 // disk
+#define MODE_PAGE_MRW                   0x03 // cdrom
+#define MODE_PAGE_RIGID_GEOMETRY        0x04
+#define MODE_PAGE_FLEXIBILE             0x05 // disk
+#define MODE_PAGE_WRITE_PARAMETERS      0x05 // cdrom
+#define MODE_PAGE_VERIFY_ERROR          0x07
+#define MODE_PAGE_CACHING               0x08
+#define MODE_PAGE_PERIPHERAL            0x09
+#define MODE_PAGE_CONTROL               0x0A
+#define MODE_PAGE_MEDIUM_TYPES          0x0B
+#define MODE_PAGE_NOTCH_PARTITION       0x0C
+#define MODE_PAGE_CD_AUDIO_CONTROL      0x0E
+#define MODE_PAGE_DATA_COMPRESS         0x0F
+#define MODE_PAGE_DEVICE_CONFIG         0x10
+#define MODE_PAGE_XOR_CONTROL           0x10 // disk
+#define MODE_PAGE_MEDIUM_PARTITION      0x11
+#define MODE_PAGE_ENCLOSURE_SERVICES_MANAGEMENT 0x14
+#define MODE_PAGE_EXTENDED              0x15
+#define MODE_PAGE_EXTENDED_DEVICE_SPECIFIC 0x16
+#define MODE_PAGE_CDVD_FEATURE_SET      0x18
+#define MODE_PAGE_PROTOCOL_SPECIFIC_LUN 0x18
+#define MODE_PAGE_PROTOCOL_SPECIFIC_PORT 0x19
+#define MODE_PAGE_POWER_CONDITION       0x1A
+#define MODE_PAGE_LUN_MAPPING           0x1B
+#define MODE_PAGE_FAULT_REPORTING       0x1C
+#define MODE_PAGE_CDVD_INACTIVITY       0x1D // cdrom
+#define MODE_PAGE_ELEMENT_ADDRESS       0x1D
+#define MODE_PAGE_TRANSPORT_GEOMETRY    0x1E
+#define MODE_PAGE_DEVICE_CAPABILITIES   0x1F
+#define MODE_PAGE_CAPABILITIES          0x2A // cdrom
+
+#define MODE_SENSE_RETURN_ALL           0x3f
+
+#define MODE_SENSE_CURRENT_VALUES       0x00
+#define MODE_SENSE_CHANGEABLE_VALUES    0x40
+#define MODE_SENSE_DEFAULT_VAULES       0x80
+#define MODE_SENSE_SAVED_VALUES         0xc0
 
 //
 // SCSI CDB operation codes
 //
 
-#define SCSIOP_TEST_UNIT_READY     0x00
-#define SCSIOP_REZERO_UNIT         0x01
-#define SCSIOP_REWIND              0x01
-#define SCSIOP_REQUEST_BLOCK_ADDR  0x02
-#define SCSIOP_REQUEST_SENSE       0x03
-#define SCSIOP_FORMAT_UNIT         0x04
-#define SCSIOP_READ_BLOCK_LIMITS   0x05
-#define SCSIOP_REASSIGN_BLOCKS     0x07
-#define SCSIOP_READ6               0x08
-#define SCSIOP_RECEIVE             0x08
-#define SCSIOP_WRITE6              0x0A
-#define SCSIOP_PRINT               0x0A
-#define SCSIOP_SEND                0x0A
-#define SCSIOP_SEEK6               0x0B
-#define SCSIOP_TRACK_SELECT        0x0B
-#define SCSIOP_SLEW_PRINT          0x0B
-#define SCSIOP_SEEK_BLOCK          0x0C
-#define SCSIOP_PARTITION           0x0D
-#define SCSIOP_READ_REVERSE        0x0F
-#define SCSIOP_WRITE_FILEMARKS     0x10
-#define SCSIOP_FLUSH_BUFFER        0x10
-#define SCSIOP_SPACE               0x11
-#define SCSIOP_INQUIRY             0x12
-#define SCSIOP_VERIFY6             0x13
-#define SCSIOP_RECOVER_BUF_DATA    0x14
-#define SCSIOP_MODE_SELECT         0x15
-#define SCSIOP_RESERVE_UNIT        0x16
-#define SCSIOP_RELEASE_UNIT        0x17
-#define SCSIOP_COPY                0x18
-#define SCSIOP_ERASE               0x19
-#define SCSIOP_MODE_SENSE          0x1A
-#define SCSIOP_START_STOP_UNIT     0x1B
-#define SCSIOP_STOP_PRINT          0x1B
-#define SCSIOP_LOAD_UNLOAD         0x1B
-#define SCSIOP_RECEIVE_DIAGNOSTIC  0x1C
-#define SCSIOP_SEND_DIAGNOSTIC     0x1D
-#define SCSIOP_MEDIUM_REMOVAL      0x1E
-#define SCSIOP_READ_CAPACITY       0x25
-#define SCSIOP_READ                0x28
-#define SCSIOP_WRITE               0x2A
-#define SCSIOP_SEEK                0x2B
-#define SCSIOP_LOCATE              0x2B
-#define SCSIOP_WRITE_VERIFY        0x2E
-#define SCSIOP_VERIFY              0x2F
-#define SCSIOP_SEARCH_DATA_HIGH    0x30
-#define SCSIOP_SEARCH_DATA_EQUAL   0x31
-#define SCSIOP_SEARCH_DATA_LOW     0x32
-#define SCSIOP_SET_LIMITS          0x33
-#define SCSIOP_READ_POSITION       0x34
-#define SCSIOP_SYNCHRONIZE_CACHE   0x35
-#define SCSIOP_COMPARE             0x39
-#define SCSIOP_COPY_COMPARE        0x3A
-#define SCSIOP_WRITE_DATA_BUFF     0x3B
-#define SCSIOP_READ_DATA_BUFF      0x3C
-#define SCSIOP_CHANGE_DEFINITION   0x40
-#define SCSIOP_READ_SUB_CHANNEL    0x42
-#define SCSIOP_READ_TOC            0x43
-#define SCSIOP_READ_HEADER         0x44
-#define SCSIOP_PLAY_AUDIO          0x45
-#define SCSIOP_PLAY_AUDIO_MSF      0x47
-#define SCSIOP_PLAY_TRACK_INDEX    0x48
-#define SCSIOP_PLAY_TRACK_RELATIVE 0x49
-#define SCSIOP_PAUSE_RESUME        0x4B
-#define SCSIOP_LOG_SELECT          0x4C
-#define SCSIOP_LOG_SENSE           0x4D
+// 6-byte commands:
+#define SCSIOP_TEST_UNIT_READY          0x00
+#define SCSIOP_REZERO_UNIT              0x01
+#define SCSIOP_REWIND                   0x01
+#define SCSIOP_REQUEST_BLOCK_ADDR       0x02
+#define SCSIOP_REQUEST_SENSE            0x03
+#define SCSIOP_FORMAT_UNIT              0x04
+#define SCSIOP_READ_BLOCK_LIMITS        0x05
+#define SCSIOP_REASSIGN_BLOCKS          0x07
+#define SCSIOP_INIT_ELEMENT_STATUS      0x07
+#define SCSIOP_READ6                    0x08
+#define SCSIOP_RECEIVE                  0x08
+#define SCSIOP_WRITE6                   0x0A
+#define SCSIOP_PRINT                    0x0A
+#define SCSIOP_SEND                     0x0A
+#define SCSIOP_SEEK6                    0x0B
+#define SCSIOP_TRACK_SELECT             0x0B
+#define SCSIOP_SLEW_PRINT               0x0B
+#define SCSIOP_SET_CAPACITY             0x0B // tape
+#define SCSIOP_SEEK_BLOCK               0x0C
+#define SCSIOP_PARTITION                0x0D
+#define SCSIOP_READ_REVERSE             0x0F
+#define SCSIOP_WRITE_FILEMARKS          0x10
+#define SCSIOP_FLUSH_BUFFER             0x10
+#define SCSIOP_SPACE                    0x11
+#define SCSIOP_INQUIRY                  0x12
+#define SCSIOP_VERIFY6                  0x13
+#define SCSIOP_RECOVER_BUF_DATA         0x14
+#define SCSIOP_MODE_SELECT              0x15
+#define SCSIOP_RESERVE_UNIT             0x16
+#define SCSIOP_RELEASE_UNIT             0x17
+#define SCSIOP_COPY                     0x18
+#define SCSIOP_ERASE                    0x19
+#define SCSIOP_MODE_SENSE               0x1A
+#define SCSIOP_START_STOP_UNIT          0x1B
+#define SCSIOP_STOP_PRINT               0x1B
+#define SCSIOP_LOAD_UNLOAD              0x1B
+#define SCSIOP_RECEIVE_DIAGNOSTIC       0x1C
+#define SCSIOP_SEND_DIAGNOSTIC          0x1D
+#define SCSIOP_MEDIUM_REMOVAL           0x1E
 
-#define MODE_PAGE_CAPABILITIES  0x2A
+// 10-byte commands
+#define SCSIOP_READ_FORMATTED_CAPACITY  0x23
+#define SCSIOP_READ_CAPACITY            0x25
+#define SCSIOP_READ                     0x28
+#define SCSIOP_WRITE                    0x2A
+#define SCSIOP_SEEK                     0x2B
+#define SCSIOP_LOCATE                   0x2B
+#define SCSIOP_POSITION_TO_ELEMENT      0x2B
+#define SCSIOP_WRITE_VERIFY             0x2E
+#define SCSIOP_VERIFY                   0x2F
+#define SCSIOP_SEARCH_DATA_HIGH         0x30
+#define SCSIOP_SEARCH_DATA_EQUAL        0x31
+#define SCSIOP_SEARCH_DATA_LOW          0x32
+#define SCSIOP_SET_LIMITS               0x33
+#define SCSIOP_READ_POSITION            0x34
+#define SCSIOP_SYNCHRONIZE_CACHE        0x35
+#define SCSIOP_COMPARE                  0x39
+#define SCSIOP_COPY_COMPARE             0x3A
+#define SCSIOP_WRITE_DATA_BUFF          0x3B
+#define SCSIOP_READ_DATA_BUFF           0x3C
+#define SCSIOP_WRITE_LONG               0x3F
+#define SCSIOP_CHANGE_DEFINITION        0x40
+#define SCSIOP_WRITE_SAME               0x41
+#define SCSIOP_READ_SUB_CHANNEL         0x42
+#define SCSIOP_READ_TOC                 0x43
+#define SCSIOP_READ_HEADER              0x44
+#define SCSIOP_REPORT_DENSITY_SUPPORT   0x44 // tape
+#define SCSIOP_PLAY_AUDIO               0x45
+#define SCSIOP_GET_CONFIGURATION        0x46
+#define SCSIOP_PLAY_AUDIO_MSF           0x47
+#define SCSIOP_PLAY_TRACK_INDEX         0x48
+#define SCSIOP_PLAY_TRACK_RELATIVE      0x49
+#define SCSIOP_GET_EVENT_STATUS         0x4A
+#define SCSIOP_PAUSE_RESUME             0x4B
+#define SCSIOP_LOG_SELECT               0x4C
+#define SCSIOP_LOG_SENSE                0x4D
+#define SCSIOP_STOP_PLAY_SCAN           0x4E
+#define SCSIOP_XDWRITE                  0x50
+#define SCSIOP_XPWRITE                  0x51
+#define SCSIOP_READ_DISK_INFORMATION    0x51
+#define SCSIOP_READ_DISC_INFORMATION    0x51 // proper use of disc over disk
+#define SCSIOP_READ_TRACK_INFORMATION   0x52
+#define SCSIOP_XDWRITE_READ             0x53
+#define SCSIOP_RESERVE_TRACK_RZONE      0x53
+#define SCSIOP_SEND_OPC_INFORMATION     0x54 // optimum power calibration
+#define SCSIOP_MODE_SELECT10            0x55
+#define SCSIOP_RESERVE_UNIT10           0x56
+#define SCSIOP_RESERVE_ELEMENT          0x56
+#define SCSIOP_RELEASE_UNIT10           0x57
+#define SCSIOP_RELEASE_ELEMENT          0x57
+#define SCSIOP_REPAIR_TRACK             0x58
+#define SCSIOP_MODE_SENSE10             0x5A
+#define SCSIOP_CLOSE_TRACK_SESSION      0x5B
+#define SCSIOP_READ_BUFFER_CAPACITY     0x5C
+#define SCSIOP_SEND_CUE_SHEET           0x5D
+#define SCSIOP_PERSISTENT_RESERVE_IN    0x5E
+#define SCSIOP_PERSISTENT_RESERVE_OUT   0x5F
 
+// 12-byte commands
+#define SCSIOP_REPORT_LUNS              0xA0
+#define SCSIOP_BLANK                    0xA1
+#define SCSIOP_ATA_PASSTHROUGH12        0xA1
+#define SCSIOP_SEND_EVENT               0xA2
+#define SCSIOP_SEND_KEY                 0xA3
+#define SCSIOP_MAINTENANCE_IN           0xA3
+#define SCSIOP_REPORT_KEY               0xA4
+#define SCSIOP_MAINTENANCE_OUT          0xA4
+#define SCSIOP_MOVE_MEDIUM              0xA5
+#define SCSIOP_LOAD_UNLOAD_SLOT         0xA6
+#define SCSIOP_EXCHANGE_MEDIUM          0xA6
+#define SCSIOP_SET_READ_AHEAD           0xA7
+#define SCSIOP_MOVE_MEDIUM_ATTACHED     0xA7
+#define SCSIOP_READ12                   0xA8
+#define SCSIOP_GET_MESSAGE              0xA8
+#define SCSIOP_SERVICE_ACTION_OUT12     0xA9
+#define SCSIOP_WRITE12                  0xAA
+#define SCSIOP_SEND_MESSAGE             0xAB
+#define SCSIOP_SERVICE_ACTION_IN12      0xAB
+#define SCSIOP_GET_PERFORMANCE          0xAC
+#define SCSIOP_READ_DVD_STRUCTURE       0xAD
+#define SCSIOP_WRITE_VERIFY12           0xAE
+#define SCSIOP_VERIFY12                 0xAF
+#define SCSIOP_SEARCH_DATA_HIGH12       0xB0
+#define SCSIOP_SEARCH_DATA_EQUAL12      0xB1
+#define SCSIOP_SEARCH_DATA_LOW12        0xB2
+#define SCSIOP_SET_LIMITS12             0xB3
+#define SCSIOP_READ_ELEMENT_STATUS_ATTACHED 0xB4
+#define SCSIOP_REQUEST_VOL_ELEMENT      0xB5
+#define SCSIOP_SEND_VOLUME_TAG          0xB6
+#define SCSIOP_SET_STREAMING            0xB6 // C/DVD
+#define SCSIOP_READ_DEFECT_DATA         0xB7
+#define SCSIOP_READ_ELEMENT_STATUS      0xB8
+#define SCSIOP_READ_CD_MSF              0xB9
+#define SCSIOP_SCAN_CD                  0xBA
+#define SCSIOP_REDUNDANCY_GROUP_IN      0xBA
+#define SCSIOP_SET_CD_SPEED             0xBB
+#define SCSIOP_REDUNDANCY_GROUP_OUT     0xBB
+#define SCSIOP_PLAY_CD                  0xBC
+#define SCSIOP_SPARE_IN                 0xBC
+#define SCSIOP_MECHANISM_STATUS         0xBD
+#define SCSIOP_SPARE_OUT                0xBD
+#define SCSIOP_READ_CD                  0xBE
+#define SCSIOP_VOLUME_SET_IN            0xBE
+#define SCSIOP_SEND_DVD_STRUCTURE       0xBF
+#define SCSIOP_VOLUME_SET_OUT           0xBF
+#define SCSIOP_INIT_ELEMENT_RANGE       0xE7
+
+// 16-byte commands
+#define SCSIOP_XDWRITE_EXTENDED16       0x80 // disk
+#define SCSIOP_WRITE_FILEMARKS16        0x80 // tape
+#define SCSIOP_REBUILD16                0x81 // disk
+#define SCSIOP_READ_REVERSE16           0x81 // tape
+#define SCSIOP_REGENERATE16             0x82 // disk
+#define SCSIOP_EXTENDED_COPY            0x83
+#define SCSIOP_RECEIVE_COPY_RESULTS     0x84
+#define SCSIOP_ATA_PASSTHROUGH16        0x85
+#define SCSIOP_ACCESS_CONTROL_IN        0x86
+#define SCSIOP_ACCESS_CONTROL_OUT       0x87
+#define SCSIOP_READ16                   0x88
+#define SCSIOP_WRITE16                  0x8A
+#define SCSIOP_READ_ATTRIBUTES          0x8C
+#define SCSIOP_WRITE_ATTRIBUTES         0x8D
+#define SCSIOP_WRITE_VERIFY16           0x8E
+#define SCSIOP_VERIFY16                 0x8F
+#define SCSIOP_PREFETCH16               0x90
+#define SCSIOP_SYNCHRONIZE_CACHE16      0x91
+#define SCSIOP_SPACE16                  0x91 // tape
+#define SCSIOP_LOCK_UNLOCK_CACHE16      0x92
+#define SCSIOP_LOCATE16                 0x92 // tape
+#define SCSIOP_WRITE_SAME16             0x93
+#define SCSIOP_ERASE16                  0x93 // tape
+#define SCSIOP_READ_CAPACITY16          0x9E
+#define SCSIOP_SERVICE_ACTION_IN16      0x9E
+#define SCSIOP_SERVICE_ACTION_OUT16     0x9F
+
+
+/****************************************************************************/
+// From inc/ddk/dsm.h
+
+#define SPTWB_SENSE_LENGTH  32
+#define SPTWB_DATA_LENGTH   512
+
+#ifdef _WIN64
+    #define PTRALIGN    DECLSPEC_ALIGN(16)
+#else
+    #define PTRALIGN    DECLSPEC_ALIGN(4)
+#endif
+
+// typedef struct _SCSI_PASS_THROUGH_WITH_BUFFERS {
+//     SCSI_PASS_THROUGH   ScsiPassThrough;
+//     PTRALIGN UCHAR      SenseInfoBuffer[SPTWB_SENSE_LENGTH];
+//     PTRALIGN UCHAR      DataBuffer[SPTWB_DATA_LENGTH];
+// } SCSI_PASS_THROUGH_WITH_BUFFERS, *PSCSI_PASS_THROUGH_WITH_BUFFERS;
+
+//  from MSDN:  enumcd.h
+//     Ramu Ramanathan     05/15/2000
 typedef struct _SCSI_PASS_THROUGH_WITH_BUFFERS {
     SCSI_PASS_THROUGH Spt;
     ULONG             Filler;      // realign buffers to double word boundary
-    UCHAR             SenseBuf[32];
-    UCHAR             DataBuf[512];
+    UCHAR             SenseBuf[SPTWB_SENSE_LENGTH];
+    UCHAR             DataBuf[SPTWB_DATA_LENGTH];
 } SCSI_PASS_THROUGH_WITH_BUFFERS, *PSCSI_PASS_THROUGH_WITH_BUFFERS;
 
+#if 0
+// Old structure from Web.
+typedef struct _SCSI_PASS_THROUGH_WITH_BUFFERS {
+    SCSI_PASS_THROUGH ScsiPassThrough;
+    ULONG             Filler;      // realign buffers to double word boundary
+    UCHAR             SenseInfoBuffer[SPTWB_SENSE_LENGTH];
+    UCHAR             DataBuffer[SPTWB_DATA_LENGTH];
+} SCSI_PASS_THROUGH_WITH_BUFFERS, *PSCSI_PASS_THROUGH_WITH_BUFFERS;
+#endif
 
-
-#endif    // _ENUMCD_H_
-
+#endif/*__scsi_h*/
