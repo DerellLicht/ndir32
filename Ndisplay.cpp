5f4101ef6d9ae29ec463cac242aedc743641f61f
Derell Licht <derelict@comcast.net>
2025-06-29 06:21:17 -0700

    wstring dirpath is now working

--- Ndisplay.cpp
+++ Ndisplay.cpp
@@ -58,16 +58,17 @@ void display_logo (void)
       nclrscr ();
    }
 
+   TCHAR vtemp[50];
    if (n.minimize) {
-      TCHAR vtemp[30];
       _stprintf(vtemp, _T("%s/%u "), ShortVersion, get_build_size());
       ngotoxy (0, _where_y ());
       nputs (n.colorlogo, vtemp);
    }
    else {
       // ngotoxy ((80 - _tcslen (Version)) / 2, _where_y ());
+      _stprintf(vtemp, _T("%s/%u "), Version, get_build_size());
       ngotoxy (0, _where_y ());
-      nputs (n.colorlogo, Version);
+      nputs (n.colorlogo, vtemp);
       ncrlf ();
    }
    // nputs(n.colordefalt, ShortVersion) ;
